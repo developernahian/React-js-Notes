0b9bf6f3153c62c4f60694f132c36462c9120acf
developernahian <developer.nahian@gmail.com>
2024-11-27 17:01:59 +0600

    conditional rendering

---
+++ 07-conditional-rendering/01-conditional-rendering/src/Task3.jsx
@@ -0,0 +1,26 @@
+//here isDone is FALSE and task is MongoDB
+
+const Task3 = ({task, isDone}) => {
+    return (
+        <>
+
+
+        {/* NOTE: True hoel print korbe */}
+        <li>{task} 😶 {isDone && 'must do it'} </li> {/*UI output: MongoDB 😶 */}
+        {/* true hole: MongoDB 😶 must do it */}
+        {/* false hole: MongoDB 😶*/}
+
+
+
+        {/* NOTE: false hole print korbe */}
+        <li>{task} 🙂 {isDone ||  'do it'} </li> {/*UI output: MongoDB 🙂 do it */}
+        {/* true hole: MongoDB 🙂 */}
+        {/* false hole: MongoDB 🙂 do it */}
+
+
+
+        </>
+    );
+};
+
+export default Task3;
