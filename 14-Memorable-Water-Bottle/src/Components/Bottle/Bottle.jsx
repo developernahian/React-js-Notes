31686901c2f091777db6675a94b3361d5df2cb65
developernahian <developer.nahian@gmail.com>
2024-12-09 22:52:40 +0600

    local storage

--- 14-Memorable-Water-Bottle/src/Components/Bottle/Bottle.jsx
+++ 14-Memorable-Water-Bottle/src/Components/Bottle/Bottle.jsx
@@ -1,13 +1,13 @@
 import './Bottle.css';
 
 const Bottle = ({bottle, handleAddToCart}) => {
-    console.log(bottle);
+    // console.log(bottle);
     
     return (
         <div className='bottle'>
             <h3>Bottle Name: {bottle.name}</h3>
             <img src={bottle.img} alt="Bottle" />
-            <p>Price: ${bottle.price}</p>
+            <p>Price: $  {bottle.price}</p>
             <p>Stock: {bottle.stock}</p>
             <button onClick={()=>handleAddToCart(bottle)}>Purchase</button>
         </div>
