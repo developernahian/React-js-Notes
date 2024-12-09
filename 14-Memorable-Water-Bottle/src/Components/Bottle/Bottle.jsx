52c198ff29c83546e44acbb55cfca68004e47be8
developernahian <developer.nahian@gmail.com>
2024-12-09 15:28:14 +0600

    cart

--- 14-Memorable-Water-Bottle/src/Components/Bottle/Bottle.jsx
+++ 14-Memorable-Water-Bottle/src/Components/Bottle/Bottle.jsx
@@ -1,6 +1,6 @@
 import './Bottle.css';
 
-const Bottle = ({bottle}) => {
+const Bottle = ({bottle, handleAddToCart}) => {
     console.log(bottle);
     
     return (
@@ -8,8 +8,8 @@ const Bottle = ({bottle}) => {
             <h3>Bottle Name: {bottle.name}</h3>
             <img src={bottle.img} alt="Bottle" />
             <p>Price: ${bottle.price}</p>
-            
             <p>Stock: {bottle.stock}</p>
+            <button onClick={()=>handleAddToCart(bottle)}>Purchase</button>
         </div>
     );
 };
--- 14-Memorable-Water-Bottle/src/Components/Bottles/Bottles.jsx
+++ 14-Memorable-Water-Bottle/src/Components/Bottles/Bottles.jsx
@@ -7,21 +7,35 @@ const Bottles = () => {
 
       //data load korte parle array er moddhe thakbe. load na korte parle empty array hobe. so default man empty array
     const [bottles, setBottles] = useState([])
+    const [cart, setCart] = useState([])
+
     useEffect(() => {
         fetch('bottles.json')
         .then(res => res.json())
         .then(data => setBottles(data))
     }, [])
 
+
+    const handleAddToCart = (bottle) => {
+        // console.log('bottle going to added');
+        // console.log(bottle);
+
+        const newCart = [...cart, bottle];
+        setCart(newCart)
+
+    }
+
     return (
         <div>
-            <h2>Total Bottle: {bottles.length}</h2>
+            <h2>Bottles Available: {bottles.length}</h2>
+            <h3>Cart: {cart.length}</h3>
             
             <div className="bottles-container">
             {
                 bottles.map(bottle => <Bottle 
                 key={bottle.id} 
                 bottle={bottle}
+                handleAddToCart={handleAddToCart}
                 ></Bottle>)
             }
             </div>
