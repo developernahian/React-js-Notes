f299548899e1492dfb802d1b26a531aca5a123c8
developernahian <developer.nahian@gmail.com>
2024-12-21 15:18:27 +0600

    add navbar

--- 22-Chefs-Table-Project/src/App.jsx
+++ 22-Chefs-Table-Project/src/App.jsx
@@ -1,12 +1,24 @@
 
 import './App.css'
+import Banner from './Components/Banner'
+import Header from './Components/Header'
+import OurRecipes from './Components/OurRecipes'
 
 function App() {
-  
+
 
   return (
     <>
-      
+      <div className='w-11/12 mx-auto'>
+        {/* Header */}
+        <Header></Header>
+        {/* Banner */}
+        <Banner></Banner>
+        {/* OurRecipes */}
+        <OurRecipes></OurRecipes>
+      </div>
+
+
     </>
   )
 }
---
+++ 22-Chefs-Table-Project/src/Components/OurRecipes.jsx
@@ -0,0 +1,11 @@
+
+
+const OurRecipes = () => {
+    return (
+        <div>
+            <h1>OurRecipes</h1>
+        </div>
+    );
+};
+
+export default OurRecipes;
