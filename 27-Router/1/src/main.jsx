18248f9a60a927599c6e241ee20582619ab29232
developernahian <developer.nahian@gmail.com>
2025-01-04 23:47:25 +0600

    update

--- 27-Router/1/src/main.jsx
+++ 27-Router/1/src/main.jsx
@@ -3,8 +3,34 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import Home from './pages/home/Home.jsx';
+import About from './pages/about/About.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  }
+])
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}>
+      <App />
+    </RouterProvider>
   </StrictMode>,
 )
+
+
+
+// navigate korar 4 ta way ache
+//Link
+//NavLink
+//Navigat
+//useNavigate
--- 27-Router/2/src/App.jsx
+++ 27-Router/2/src/App.jsx
@@ -2,11 +2,10 @@
 import './App.css'
 
 function App() {
-
+  
 
   return (
     <>
-      
       
     </>
   )
